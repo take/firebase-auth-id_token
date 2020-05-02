6dd17a0d891ceb338991d9e3700732694b0901dd
Takehiro Adachi <takehiro0740@gmail.com>
2020-05-02 17:51:24 +0900

    This is pretty obvious

--- spec/firebase/auth/id_token_spec.rb
+++ spec/firebase/auth/id_token_spec.rb
@@ -1,8 +1,4 @@
 RSpec.describe Firebase::Auth::IDToken do
-  it "has a version number" do
-    expect(Firebase::Auth::IDToken::VERSION).not_to be nil
-  end
-
   describe '#verify!' do
     context 'when the given ID token' do
       context 'cannot be decoded by JWT' do
