7ac948d5003d5c9b082f4eb4491a5046263c30f1
Takehiro Adachi <takehiro0740@gmail.com>
2020-05-02 17:31:59 +0900

    Implement config

--- lib/firebase/auth/id_token.rb
+++ lib/firebase/auth/id_token.rb
@@ -1,10 +1,20 @@
-require "firebase/auth/id_token/version"
+require 'firebase/auth/id_token/version'
+require 'firebase/auth/id_token/config'
 
 module Firebase
   module Auth
     module IDToken
       class Error < StandardError; end
-      # Your code goes here...
+
+      class << self
+        def configure
+          yield config
+        end
+
+        def config
+          @config ||= Config.new
+        end
+      end
     end
   end
 end
---
+++ lib/firebase/auth/id_token/config.rb
@@ -0,0 +1,9 @@
+module Firebase
+  module Auth
+    module IDToken
+      class Config
+        attr_accessor :firebase_project_id
+      end
+    end
+  end
+end
